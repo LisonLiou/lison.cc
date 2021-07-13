593924c1b87920cabd06fa3df3fab0f97d9e554f
lison <lison.liou@yahoo.com>
2021-07-13 10:28:02 +0800

    Site updated: 2021-07-13 10:27:53

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?487d03fcccff80b996cf94eaf236695e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
